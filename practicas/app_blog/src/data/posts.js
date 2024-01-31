2ce33587fc02acd3ecbd2ef20d482bd7641c5a99
abelgh91 <abelgh91@gmail.com>
2024-01-31 10:49:40 +0100

    react router dom

---
+++ practicas/app_blog/src/data/posts.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        id: 1,
+        titulo: 'Artículo #1',
+        texto: 'Este es el texto de mi primer artículo'
+    },
+    {
+        id: 2,
+        titulo: 'Artículo #2',
+        texto: 'Este es el texto de mi segundo artículo'
+    },
+    {
+        id: 3,
+        titulo: 'Artículo #3',
+        texto: 'Este es el texto de mi tercer artículo'
+    },
+];
