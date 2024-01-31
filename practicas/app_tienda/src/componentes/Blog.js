9edf6668e35bf184c06ef310e205a35e1ed63a17
abelgh91 <abelgh91@gmail.com>
2024-01-31 16:58:52 +0100

    app carrito

---
+++ practicas/app_tienda/src/componentes/Blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Blog = () => {
+    return ( 
+        <div>
+            <h1>Blog</h1>
+            <p>Lorem ipsum dolor sit amet....</p>
+        </div>
+     );
+}
+ 
+export default Blog;
---
+++ practicas/app_tienda/src/componentes/Inicio.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Inicio = () => {
+    return ( 
+        <div>
+            <h1>Inicio</h1>
+            <p>Lorem ipsum dolor sit amet....</p>
+        </div>
+     );
+}
+ 
+export default Inicio;
