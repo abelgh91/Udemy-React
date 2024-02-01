860fd91d36a94eab22d8e7f90375b23c7c575706
abelgh91 <abelgh91@gmail.com>
2024-02-01 14:53:03 +0100

    app tienda

--- practicas/app_tienda/src/App.js
+++ practicas/app_tienda/src/App.js
@@ -20,6 +20,42 @@ const App = () => {
 
 const [carrito, cambiarCarrito] = useState([]);
 
+const agregarProductoAlCarrito = (idProductoAAgregar, nombre) => {
+  // Si el carrito no tiene elementos entonces agregamos uno.
+  if(carrito.length === 0){
+    cambiarCarrito([{id: idProductoAAgregar, nombre: nombre, cantidad: 1}])
+  }else{
+    // De otra foma tenemos que revisar que el carrito no tenga ya el producto que queremos agregar.
+    // Si ya lo tiene entonces queremos actualizar su valor.
+    // Si no tiene el producto entonces lo agregamos.
+
+    // Para poder editar el arreglo tenemos que clonarlo.
+    const nuevoCarrito = [...carrito];
+
+    // Comprobamos si el carrito ya tiene el ID del producto a agregar.
+    const yaEstaEnCaarito = nuevoCarrito.filter((productoDeCarrito) => {
+      return productoDeCarrito.id === idProductoAAgregar
+    }).length > 0;
+
+    // Si ya tiene el producto entonces lo tenemos que actualizar.
+    if(yaEstaEnCaarito){
+      // Para ello tenemos que buscarlo, obtener su posicion en el arreglo.
+      // Y en base a su posicion ya actualizamos el valor.
+      nuevoCarrito.forEach((productoDeCarrito, index) => {
+        if(productoDeCarrito.id === idProductoAAgregar){
+          const cantidad = nuevoCarrito[index].cantidad;
+          nuevoCarrito[index] = {id: idProductoAAgregar, nombre: nombre, cantidad: cantidad + 1}
+        }
+      });
+    }else{
+      // De otra forma entonces agregamos el producto al arreglo.
+      nuevoCarrito.push({id: idProductoAAgregar, nombre: nombre, cantidad: 1});
+    }
+    // Por ultimo actualizamos el carrito.
+      cambiarCarrito(nuevoCarrito);
+  }
+}
+
   return ( 
     <>
       <Contenedor>
@@ -36,6 +72,7 @@ const [carrito, cambiarCarrito] = useState([]);
             <Route path="/tienda" element={
                         <Tienda 
                             productos={productos} 
+                            agregarProductoAlCarrito={agregarProductoAlCarrito}
                         />
                     } />
           </Routes>
