83a5c91868ac697a39d3e5ea6c9d65f53ff49514
abelgh91 <abelgh91@gmail.com>
2024-02-05 12:43:49 +0100

    inicio proyecto

---
+++ practicas/app_lista_gastos/src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import WebFont from 'webfontloader'
+import Contenedor from './elementos/Contenedor';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import EditarGasto from './componentes/EditarGastos';
+import GastosPorCategoria from './componentes/GastosPorCategorias';
+import InicioSesion from './componentes/InicioSesion';
+import ListaDeGastos from './componentes/ListaDeGastos';
+import RegistroUsuarios from './componentes/RegistroUsuarios';
+
+
+WebFont.load({
+  google: {
+    families: ['Work Sans:400,500,700', 'sans-serif']
+  }
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Contenedor>
+        <Routes>
+          <Route path="/iniciar-sesion" element={<InicioSesion/>} />
+          <Route path="/crear-cuenta" element={<RegistroUsuarios/>} />
+          <Route path="/categorias" element={<GastosPorCategoria/>} />
+          <Route path="/lista" element={<ListaDeGastos/>} />
+          <Route path="/editar/:id" element={<EditarGasto/>} />
+          <Route path="/" element={<App/>} />
+        </Routes>
+      </Contenedor>
+    </BrowserRouter>
+  </React.StrictMode>
+);
