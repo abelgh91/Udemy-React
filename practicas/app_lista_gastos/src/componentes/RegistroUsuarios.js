5932e5b4a4508e1d25599c9a9123e5f4a7b8f7be
abelgh91 <abelgh91@gmail.com>
2024-02-07 10:41:42 +0100

    proyecto

--- practicas/app_lista_gastos/src/componentes/RegistroUsuarios.js
+++ practicas/app_lista_gastos/src/componentes/RegistroUsuarios.js
@@ -5,6 +5,9 @@ import Boton from '../elementos/Boton'
 import {ContenedorBoton, Input, Formulario} from './../elementos/ElementosDeFormulario'
 import {ReactComponent as SvgLogin} from './../imagenes/registro.svg'
 import styled from 'styled-components'
+import { auth } from '../firebase/firebaseConfig'
+import { createUserWithEmailAndPassword } from 'firebase/auth' 
+import { useNavigate } from 'react-router-dom'
 
 
 const Svg = styled(SvgLogin)`
@@ -14,13 +17,69 @@ margin-bottom: 1.25rem;
 `;
 
 const RegistroUsuarios = () => {
-
+    const navigate = useNavigate();
     const [correo, establecerCorreo] = useState('')
     const [password, establecerPassword] = useState('')
     const [password2, establecerPassword2] = useState('')
 
     const handleChange = (e) => {
-        console.log(e)
+        switch(e.target.name){
+            case 'email':
+                establecerCorreo(e.target.value);
+                break;
+            case 'password':
+                establecerPassword(e.target.value);
+                break;
+            case 'password2':
+                establecerPassword2(e.target.value);
+                break;
+            default:
+                break;
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        //Comprobamos del lado del cliente que el correo es valido
+        const expresionRegular = /[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+/;
+        if(!expresionRegular.test(correo)){
+            console.log('pon un correo válido');
+            return;
+        }
+
+        if(correo === '' || password === '' || password2 === ''){
+            console.log('por favor, rellene todos los datos');
+            return;
+        }
+
+        if(password !== password2){
+            console.log('las contraseñas no coinciden');
+            return;
+        }
+
+        try {
+            await createUserWithEmailAndPassword(auth, correo, password)
+            navigate('/');
+            
+        } catch (error) {
+            let mensaje;
+            switch(error.code){
+                case 'auth/invalid-password':
+                    mensaje = 'La contraseña tiene que ser de al menos 6 caracteres'
+                    break;
+                case 'auth/email-already-in-use':
+                    mensaje = 'Ya existe una cuenta con el correo electrónico proporcionado.'
+                    break;
+                case 'auth/invalid-email':
+                    mensaje = 'El correo electrónico no es válido.'
+                    break;
+                default:
+                    mensaje = 'Hubo un error al intentar crear la cuenta.'
+                break;
+            }
+            console.log(mensaje)
+        }
     }
 
     return ( 
@@ -38,11 +97,11 @@ const RegistroUsuarios = () => {
                     </div>
                 </ContenedorHeader>
             </Header>
-            <Formulario>
+            <Formulario onSubmit={handleSubmit} >
                 <Svg />
                 <Input type="email" name="email" placeholder="Correo Electrónico" value={correo} onChange={handleChange} />
                 <Input type="password" name="password" placeholder="Contraseña" value={password} onChange={handleChange} />
-                <Input type="password2" name="password2" placeholder="Repetir Contraseña" value={password2} onChange={handleChange} />
+                <Input type="password" name="password2" placeholder="Repetir Contraseña" value={password2} onChange={handleChange} />
                 <ContenedorBoton>
                     <Boton as="button" primario="true" type="submit">Crear Cuenta</Boton>
                 </ContenedorBoton>
